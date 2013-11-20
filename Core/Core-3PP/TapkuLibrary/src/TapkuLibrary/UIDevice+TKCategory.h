b3f7e5a78f665dc62b0f6541a586d91551483264
Tom Callon <tomcallon2010@gmail.com>
2013-11-21 02:41:29 +0800

    updates

---
+++ Core/Core-3PP/TapkuLibrary/src/TapkuLibrary/UIDevice+TKCategory.h
@@ -0,0 +1,16 @@
+//
+//  UIDevice+TKCategory.h
+//  TapkuLibrary
+//
+//  Created by Devin Ross on 8/22/13.
+//
+//
+
+@import UIKit;
+
+@interface UIDevice (TKCategory)
+
++ (BOOL) padIdiom;
++ (BOOL) phoneIdiom;
+
+@end
